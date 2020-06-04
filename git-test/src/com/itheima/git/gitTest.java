540cc587619414169ca0ce5089294327fc205c30
admin <admin@163.com>
2020-06-04 14:09:05 +0800

    本地工厂添加到git

---
+++ git-test/src/com/itheima/git/gitTest.java
@@ -0,0 +1,10 @@
+package com.itheima.git;
+
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello git");
+        System.out.println("Hello git");
+        System.out.println("Hello git");
+        System.out.println("Hello git");
+    }
+}
